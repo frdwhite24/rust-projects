f55cf5be19581805157d1df3082a4908fb028117
Fred White <fred-white@hotmail.co.uk>
2021-12-04 16:40:54 +0000

    feat(exercism rust exercises): add new exercise platform for rust

---
+++ rust/assembly-line/src/lib.rs
@@ -0,0 +1,19 @@
+pub fn production_rate_per_hour(speed: u8) -> f64 {
+    const COUNT_PER_SPEED_UNIT: u8 = 221;
+
+    let success_rate = if speed >= 1 && speed < 5 {
+        1.0
+    } else if speed >= 5 && speed < 9 {
+        0.9
+    } else if speed >= 9 && speed < 11 {
+        0.77
+    } else {
+        0 as f64
+    };
+
+    speed as f64 * COUNT_PER_SPEED_UNIT as f64 * success_rate
+}
+
+pub fn working_items_per_minute(speed: u8) -> u32 {
+    (production_rate_per_hour(speed) / 60 as f64).floor() as u32
+}
---
+++ rust/semi-structured-logs/src/lib.rs
@@ -0,0 +1,32 @@
+#[derive(Clone, PartialEq, Debug)]
+pub enum LogLevel {
+    Debug,
+    Info,
+    Warning,
+    Error,
+}
+
+pub fn log(level: LogLevel, message: &str) -> String {
+    match level {
+        LogLevel::Debug => debug(message),
+        LogLevel::Info => info(message),
+        LogLevel::Warning => warn(message),
+        LogLevel::Error => error(message),
+    }
+}
+
+pub fn debug(message: &str) -> String {
+    format!("[DEBUG]: {}", message)
+}
+
+pub fn info(message: &str) -> String {
+    format!("[INFO]: {}", message)
+}
+
+pub fn warn(message: &str) -> String {
+    format!("[WARNING]: {}", message)
+}
+
+pub fn error(message: &str) -> String {
+    format!("[ERROR]: {}", message)
+}
