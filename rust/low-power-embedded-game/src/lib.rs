41af0c39dc1d1cd22c1f876bd0addc27dd6dbdc6
Fred White <fred-white@hotmail.co.uk>
2021-12-05 19:50:37 +0000

    feat(rust exercises): low power embedded game learning destructuring and iterators

---
+++ rust/low-power-embedded-game/src/lib.rs
@@ -0,0 +1,17 @@
+pub fn divmod(dividend: i16, divisor: i16) -> (i16, i16) {
+    (dividend / divisor, dividend % divisor)
+}
+
+pub fn evens<T>(iter: impl Iterator<Item = T>) -> impl Iterator<Item = T> {
+    iter.enumerate()
+        .filter(|(index, _value)| index % 2 == 0)
+        .map(|(_index, value)| value)
+}
+
+pub struct Position(pub i16, pub i16);
+
+impl Position {
+    pub fn manhattan(&self) -> i16 {
+        self.0.abs() + self.1.abs()
+    }
+}
